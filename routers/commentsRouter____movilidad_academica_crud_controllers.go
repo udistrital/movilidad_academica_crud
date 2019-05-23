42ed8084bba94907c8d6a7801ef9e2fa4b46088f
fabianbarreto02 <fabibeto11@hotmail.com>
2019-05-23 16:24:14 -0500

    Se sube el crud

---
+++ routers/commentsRouter____movilidad_academica_crud_controllers.go
@@ -0,0 +1,145 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:MovilidadController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:MovilidadController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:MovilidadController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:MovilidadController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:MovilidadController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:MovilidadController"],
+        beego.ControllerComments{
+            Method: "GetOne",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:MovilidadController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:MovilidadController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:MovilidadController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:MovilidadController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoCategoriaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoCategoriaController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoCategoriaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoCategoriaController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoCategoriaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoCategoriaController"],
+        beego.ControllerComments{
+            Method: "GetOne",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoCategoriaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoCategoriaController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoCategoriaController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoCategoriaController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoMovilidadController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoMovilidadController"],
+        beego.ControllerComments{
+            Method: "Post",
+            Router: `/`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoMovilidadController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoMovilidadController"],
+        beego.ControllerComments{
+            Method: "GetAll",
+            Router: `/`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoMovilidadController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoMovilidadController"],
+        beego.ControllerComments{
+            Method: "GetOne",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoMovilidadController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoMovilidadController"],
+        beego.ControllerComments{
+            Method: "Put",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"put"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoMovilidadController"] = append(beego.GlobalControllerRouter["github.com/udistrital/movilidad_academica_crud/controllers:TipoMovilidadController"],
+        beego.ControllerComments{
+            Method: "Delete",
+            Router: `/:id`,
+            AllowHTTPMethods: []string{"delete"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+}
